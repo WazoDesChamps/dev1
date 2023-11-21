d6d17743cdca76b546c9c8695ff2c2376e2ab140
60415 <60415@etu.he2b.be>
2023-11-21 11:38:58 +0100

    Exercice 1 td8

---
+++ td8/ex1.py
@@ -0,0 +1,14 @@
+nombres_impairs = list(range(1, 10, 2))
+
+for i in (nombres_impairs):
+    print(i)
+
+print()
+
+for i in (0, 0.25, 0.5, 0.75,1):
+    print(i)
+
+print()
+
+for i in ("une", "boucle", "for"):
+    print(i)
