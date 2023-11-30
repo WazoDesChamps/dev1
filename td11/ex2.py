25fd86372ecc484394e5e37a1debe275b3fccfac
Wazo_des_champs <arthur.cormann@gmail.com>
2023-11-30 18:39:40 +0100

    ex2 fin / ex3 ???

--- td11/ex2.py
+++ td11/ex2.py
@@ -1,18 +1,24 @@
 import time
 
 #Ajout des 5_000_000 de valeurs
-value = 0
 a_tuple = ()
+total_time = 0
+
 for i in range(5_000_000):
     a_tuple = a_tuple + (i,)
 
-#calcul de la somme des éléments dans le tuple
-start_time = time.time() 
-for j in range(len(a_tuple)):
-    value = value + a_tuple[j]
-end_time = time.time()
+for k in range(50):
+    value = 0
+    #calcul de la somme des éléments dans le tuple
+    start_time = time.time() 
+    for j in range(len(a_tuple)):
+        value = value + a_tuple[j]
+    end_time = time.time()
+    acces_time = end_time - start_time
+    print(value, acces_time)
 
-#Calcul du temps
-acces_time = end_time - start_time
+    total_time += acces_time
 
-print("Le temps pour calculer la somme de tous les éléments est de", acces_time)
+#Calcul du temps moyen
+average = total_time / 50
+print("Average:", average,"s")
---
+++ td11/ex3.py
@@ -0,0 +1,10 @@
+# Tuple initial
+a_tuple = (15, 18, 12, 14, 20, 10, 16, 19, 9, 13)
+
+# Tri du tuple et affichage sans la plus petite et la plus grande valeur
+b_tuple = sorted(a_tuple)
+
+# Affichage du résultat
+print(b_tuple)
+
+#???????
