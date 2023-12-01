7e6925f8b1f72fe660bc86435944864003683b63
Wazo_des_champs <arthur.cormann@gmail.com>
2023-12-01 23:01:07 +0100

    Création du plateau

---
+++ td11/Une_seule_ligne.py
@@ -0,0 +1,20 @@
+#Ajout des différents modules
+import random
+
+##########################
+#Declartion des variables#
+##########################
+
+#Création de notre liste plateau
+plateau = []
+
+#Ajout des 20 cases dans le plateau
+for i in range(20):
+    plateau.append(0)
+
+#Valeurs possibles à utiliser durant la partie
+valeurs = random.randint(0, 4)
+
+#########################
+#Fonctions et programme #
+#########################
