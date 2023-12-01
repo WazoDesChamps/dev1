2ef3dee4ddb44761344ef1632fe0b4cf917a9704
Wazo_des_champs <arthur.cormann@gmail.com>
2023-12-01 23:13:29 +0100

    Function add_2_values

--- td11/Une_seule_ligne.py
+++ td11/Une_seule_ligne.py
@@ -6,15 +6,28 @@ import random
 ##########################
 
 #Création de notre liste plateau
-plateau = []
+_plateau = []
 
 #Ajout des 20 cases dans le plateau
 for i in range(20):
-    plateau.append(0)
+    _plateau.append(0)
 
 #Valeurs possibles à utiliser durant la partie
-valeurs = random.randint(0, 4)
+_valeurs = random.randint(0, 4)
 
 #########################
 #Fonctions et programme #
 #########################
+
+#Ajoute 2 valeurs aléatoire sur le plateau
+def add_2_values():
+    _pos_1 = random.randint(0, 20)
+    while(_plateau[_pos_1] != 0):
+        _pos_1 = random.randint(0, 20)
+
+    _pos_2 = random.randint(0, 20)
+    while(_plateau[_pos_2] != 0):
+        _pos_2 = random.randint(0, 20)
+
+    _plateau.insert(_pos_1, _valeurs)
+    _plateau.insert(_pos_2, _valeurs)
---
+++ td11/ex5.py
@@ -0,0 +1,18 @@
+a_list = [1, 2, 3, 4, 5]
+b_list = [6, 7, 8, 9, 10]
+c_list = []
+
+print("========================")
+print("EXEMPLE DE L'OPERATEUR +")
+print("========================")
+
+c_list = a_list + b_list
+print(c_list)
+print("")
+
+print("=============================")
+print("EXEMPLE DE L'OPERATEUR EXTEND")
+print("=============================")
+
+a_list.extend(b_list)
+print(a_list)
