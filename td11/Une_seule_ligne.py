da0691b8fe78952ddcf0d059c1467b5f14b6eddb
Wazo_des_champs <arthur.cormann@gmail.com>
2023-12-01 23:21:21 +0100

    Function display

--- td11/Une_seule_ligne.py
+++ td11/Une_seule_ligne.py
@@ -12,22 +12,26 @@ _plateau = []
 for i in range(20):
     _plateau.append(0)
 
-#Valeurs possibles à utiliser durant la partie
-_valeurs = random.randint(0, 4)
-
 #########################
 #Fonctions et programme #
 #########################
 
 #Ajoute 2 valeurs aléatoire sur le plateau
 def add_2_values():
-    _pos_1 = random.randint(0, 20)
+    _pos_1 = random.randint(0, 19)
     while(_plateau[_pos_1] != 0):
-        _pos_1 = random.randint(0, 20)
+        _pos_1 = random.randint(0, 19)
 
-    _pos_2 = random.randint(0, 20)
+    _pos_2 = random.randint(0, 19)
     while(_plateau[_pos_2] != 0):
-        _pos_2 = random.randint(0, 20)
+        _pos_2 = random.randint(0, 19)
 
-    _plateau.insert(_pos_1, _valeurs)
-    _plateau.insert(_pos_2, _valeurs)
+    _plateau.insert(_pos_1, random.randint(1, 5))
+    _plateau.insert(_pos_2, random.randint(1, 5))
+
+#Permet d'afficher le plateau
+def display():
+    print(_plateau)
+
+add_2_values()
+display()
