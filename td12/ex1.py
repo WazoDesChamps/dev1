4f156269b1171667df15cacc9f78c53be8bd9ba0
Wazo_des_champs <arthur.cormann@gmail.com>
2023-12-06 16:31:54 +0100

    Ajout du dossier td12

---
+++ td12/ex1.py
@@ -0,0 +1,8 @@
+import random
+ 
+liste1 = []
+
+for i in range(10):
+    liste1.append(random.randint(0, 9))
+
+print(liste1)
---
+++ td12/ex2.py
@@ -0,0 +1,4 @@
+mots = ['Carottes', 'Poireaux', 'Tomates', 'Aubergines', 'Courgettes']
+mots = mots.append("Brocolis")
+
+print(mots)
