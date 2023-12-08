51d1db84803f52b50a4518e32558a1430bb49ec7
Wazo_des_champs <arthur.cormann@gmail.com>
2023-12-08 09:40:46 +0100

    Suite ex2.py

--- td13/ex2.py
+++ td13/ex2.py
@@ -1,3 +1,4 @@
+#Dictionnaires
 profs_dev1 = {
     "A111": "bej", "A112": "mbr", "A121": "sre", "A122": "dbo", "A131": "abe",
     "A132": "clg", "A211": "bej", "A212": "cuv", "A221": "sre", "A222": "sdr",
@@ -5,10 +6,33 @@ profs_dev1 = {
     "A322": "hal", "A331": "srv", "A332": "gba", "A341": "pbt", "A342": "tni"
 }
 
+etudiants = {
+    52104: {
+        "prénom": "Guillaume",
+        "nom": "Apollinaire",
+        "groupe": "A321"
+    },
+    45371: {
+        "prénom": "Jean-Jacques",
+        "nom": "Goldmann",
+        "groupe": "A123"
+    }
+}
+
 # obtenir le prof du groupe A311
 for k in profs_dev1:
     if k == "A311":
-        prof = profs_dev1[k]
-        break
+        print(profs_dev1[k])
 
-print(profs_dev1[k])
+#▷ obtenir le nombre de groupes de Mme Cuvelier (cuv).
+cpt = 0
+for k, v in profs_dev1.items():
+    if v == "mbr":
+        cpt += 1
+
+print("cuv a", cpt, "groupe.")
+
+#▷ obtenir le groupe de l’étudiant ayant le matricule 52104
+print(etudiants.get(52104, {}).get("groupe", {}))
+
+#▷ afficher tou·tes les enseignant·es de dev1 (sans doublons)
